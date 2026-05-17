a6c9dbb0e2b48144f9d092990ef56e23160a51bd
Jason <orangegjs@gmail.com>
2026-05-17 12:22:03 +0800

    feat: make role searcable in search bar

--- backend/controls/search_user_account_controller.py
+++ backend/controls/search_user_account_controller.py
@@ -1,12 +1,19 @@
 from sqlalchemy import String
 from sqlalchemy.orm import Session
 from entities.user_account import UserAccount
+from entities.user_profile import UserProfile
 
 
 class SearchUserAccountController:
     def searchUserAcc(self, db: Session, keyword: str) -> list:
-        results = db.query(UserAccount).filter(
-            UserAccount.username.ilike(f"%{keyword}%")
-            | UserAccount.userID.cast(String).ilike(f"%{keyword}%")
-        ).all()
+        results = (
+            db.query(UserAccount)
+            .outerjoin(UserProfile, UserAccount.profile_id == UserProfile.profileID)
+            .filter(
+                UserAccount.username.ilike(f"%{keyword}%")
+                | UserAccount.userID.cast(String).ilike(f"%{keyword}%")
+                | UserProfile.profileName.ilike(f"%{keyword}%")
+            )
+            .all()
+        )
         return results
